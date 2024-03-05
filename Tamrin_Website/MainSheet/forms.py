3fa00361c32c8a4cf640ce814456d37d2caefe80
FahimReza <Fahimreza202@outlook.com>
2024-03-05 21:36:38 +0330

    End The Changes User

--- Tamrin_Website/MainSheet/forms.py
+++ Tamrin_Website/MainSheet/forms.py
@@ -8,7 +8,7 @@ class LoginForm(forms.Form):
             item.field.widget.attrs["class"] = "form-control"
             item.field.widget.attrs["style"] = "text-align:center;"
 
-    id=forms.IntegerField(widget=forms.HiddenInput , required=True)
+    id=forms.IntegerField(widget=forms.HiddenInput , label="")
     FullName=forms.CharField(max_length=50 , required=True , label="نام و نام خانوادگی" ,)
     Email=forms.CharField(max_length=50 , required=True , widget=forms.EmailInput()  , label="ایمیل")
     Password=forms.CharField(max_length=50 , required=True , widget=forms.PasswordInput , label="رمز ورود")
