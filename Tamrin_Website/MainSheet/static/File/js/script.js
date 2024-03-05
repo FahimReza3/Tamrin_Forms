8c308ae004ef09adc50fe4149d6be8b8faf7bbda
FahimReza <Fahimreza202@outlook.com>
2024-03-05 18:22:08 +0330

    Save Change in the  MainSheet

---
+++ Tamrin_Website/MainSheet/static/File/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    alert("سلام ، به شرکت مای وبسایت خوش آمدید")
+    
+});
+
+
+$("#Karbord-img").click(function() {
+    $(".Modal").fadeToggle();
+});
--- Tamrin_Website/MainSheet/views.py
+++ Tamrin_Website/MainSheet/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Main(request):
+    return render(request=request , template_name="Main.html" , context={})
