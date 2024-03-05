16891accef7979f8db6cf2ca6aea14dd3dd2a7a5
FahimReza <Fahimreza202@outlook.com>
2024-03-05 19:20:22 +0330

    Save Changes ViewUser and AddUser

--- Tamrin_Website/MainSheet/forms.py
+++ Tamrin_Website/MainSheet/forms.py
@@ -2,6 +2,12 @@ from django import forms
 
 
 class LoginForm(forms.Form):
+    def __init__(self , *args , **kwargs):
+        super(LoginForm ,self).__init__(*args , **kwargs)
+        for item in LoginForm.visible_fields(self):
+            item.field.widget.attrs["class"] = "form-control"
+
+
     FullName=forms.CharField(max_length=50 , required=True , label="نام و نام خانوادگی")
     Email=forms.CharField(max_length=50 , required=True , widget=forms.EmailInput  , label="ایمیل")
     Password=forms.CharField(max_length=50 , required=True , widget=forms.PasswordInput , label="رمز ورود")
