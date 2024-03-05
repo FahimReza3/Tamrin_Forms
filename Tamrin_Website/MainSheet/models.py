4c3c03988af7ee1f71dce10b158a50df4eac94ae
FahimReza <Fahimreza202@outlook.com>
2024-03-05 18:26:49 +0330

    Create a DataBase

--- Tamrin_Website/MainSheet/models.py
+++ Tamrin_Website/MainSheet/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    id=models.IntegerField(primary_key=True , unique=True)
+    FullName=models.CharField(max_length=50)
+    Email=models.CharField(max_length=50)
+    Password=models.CharField(max_length=50)
--- Tamrin_Website/MainSheet/static/File/js/script.js
+++ Tamrin_Website/MainSheet/static/File/js/script.js
@@ -1,8 +1,3 @@
-$(document).ready(function() {
-    alert("سلام ، به شرکت مای وبسایت خوش آمدید")
-    
-});
-
 
 $("#Karbord-img").click(function() {
     $(".Modal").fadeToggle();
