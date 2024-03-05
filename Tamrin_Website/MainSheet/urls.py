e7edc2fcd9a024a4c0a564276b6209e219a7c4dd
FahimReza <Fahimreza202@outlook.com>
2024-03-05 19:34:25 +0330

    Create DeletUser

--- Tamrin_Website/MainSheet/urls.py
+++ Tamrin_Website/MainSheet/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('' , views.Main , name="MainSheet"),
     path('AddUser' , views.AddUser , name="AddUser Page"),
     path('ViewUser' , views.ViewUser , name="ViewUser Page"),
+    path('DeletUser/<int:id>' , views.DeletUser , name="ViewUser Page"),
 ]
--- Tamrin_Website/MainSheet/views.py
+++ Tamrin_Website/MainSheet/views.py
@@ -7,17 +7,22 @@ from django.http import HttpResponse,HttpResponseRedirect
 def Main(request):
     return render(request=request , template_name="Main.html" , context={})
 def AddUser(request):
+    msg=""
     if request.method == "POST":
         forms=LoginForm(request.POST)
         data=User(FullName=forms.data["FullName"] , Email=forms.data["Email"] , Password=forms.data["Password"])
         data.save()
-        return HttpResponse("کاربر ثبت شد")
-
+        msg="کاربر با موفقیت ثبت شد"
     else:
         forms=LoginForm()
 
-    return render(request=request , template_name="SignUp.html" , context={"Form" : forms})
+    return render(request=request , template_name="SignUp.html" , context={"Form" : forms , "Message" : msg})
 
 def ViewUser(request):
     DataBase=User.objects.all()
     return render(request=request , template_name="TableData.html" , context={"DataBase" : DataBase})
+
+def DeletUser(request , id):
+    Data=User.objects.filter(id=id).first()
+    Data.delete()
+    return HttpResponseRedirect("/ViewUser")
